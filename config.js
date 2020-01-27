5194d8156b472f253ca6b910a7acf47c0c60da4d
Luis Caballero <luis_cn98@hotmail.com>
2020-01-27 07:42:50 -0600

    Added comment string

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
+//mongodb+srv://luis:<password>@cluster0-g8ctf.mongodb.net/test?retryWrites=true&w=majority
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/university";
 exports.PORT = process.env.PORT || 8080;
